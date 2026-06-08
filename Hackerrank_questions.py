79645b5f10054f4aaab907e08dd25b38e04b4f25
Aditya sinha <aditya2107sinha@gmail.com>
2026-06-08 13:18:12 +0530

    Hackerrank - sets and solve

--- Hackerrank_questions.py
+++ Hackerrank_questions.py
@@ -19,8 +19,6 @@ for _ in range(t):
 
 n , m = input().split()
 array = list(map(int,input().split()))
-
-
 A = set(map(int,input().split()))
 B = set(map(int,input().split()))
 happiness = 0
@@ -31,3 +29,15 @@ for i in array:
     elif i in B:
         happiness -= 1
 print(happiness)
+
+
+
+N = int(input())
+require_set = set()
+for i in range(N):
+    name = input()
+    if name not in require_set:
+        require_set.add(name)
+
+
+print(len(require_set))
