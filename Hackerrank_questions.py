0ba81932391c32c8a253463401a9dc6b4ecafca4
Aditya sinha <aditya2107sinha@gmail.com>
2026-06-08 13:00:52 +0530

    HackerRank medium - solved!

--- Hackerrank_questions.py
+++ Hackerrank_questions.py
@@ -16,3 +16,18 @@ for _ in range(t):
         print(True)
     except re.error:
         print(False)
+
+n , m = input().split()
+array = list(map(int,input().split()))
+
+
+A = set(map(int,input().split()))
+B = set(map(int,input().split()))
+happiness = 0
+
+for i in array:
+    if i in A:
+        happiness += 1
+    elif i in B:
+        happiness -= 1
+print(happiness)
